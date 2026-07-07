bca134502dcebbf1cb538ce8f0d1d52489fef653
Jonathan Lambert <jonathanlambert247@gmail.com>
2026-07-06 19:09:34 -0700

    add volume control for hurt sound, and export presets

--- scripts/sfx.gd
+++ scripts/sfx.gd
@@ -1,10 +1,12 @@
 extends Node
 
 var hit_sound = preload("res://assets/hit_thud.wav")
+var hit_volume: float = -25.0
 
 func play_hit():
 	var p = AudioStreamPlayer.new()
 	p.stream = hit_sound
+	p.volume_db = hit_volume
 	add_child(p)
 	p.play()
 	p.finished.connect(p.queue_free)
